469be3d0fbdc33a018451e2df0f9cd278ae49e6d
Tiger Liu <34651180+Tiger101010@users.noreply.github.com>
2022-04-24 15:42:13 -0700

    init project (#1)
    * init
    
    * fix: schemal
    
    * switch login and register page
    
    * tide up
    
    Co-authored-by: Christina Chen <wenjunc3@uci.edu>

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # BankWebApp
+This is a bank website built for SWE266P
+___
+
+Install
+-------
+
+Create a virtualenv and activate it::
+
+    $ python3 -m venv venv
+    $ . venv/bin/activate
+
+Or on Windows cmd::
+
+    $ py -3 -m venv venv
+    $ venv\Scripts\activate.bat
+
+Install Flaskr::
+
+    $ pip install -r requirements.txt
+
+Run
+---
+
+::
+
+    $ export FLASK_APP=flaskr
+    $ export FLASK_ENV=development
+    $ flask init-db
+    $ flask run
+
+Or on Windows cmd::
+
+    > set FLASK_APP=flaskr
+    > set FLASK_ENV=development
+    > flask init-db
+    > flask run
+
+Open http://127.0.0.1:5000 in a browser.
---
+++ flaskr/__init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+from flask import render_template
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def index():
+        return render_template("base.html")
+
+    from flaskr import db
+    db.init_app(app)
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+    app.register_blueprint(auth.bp)
+    app.add_url_rule("/", endpoint="index")
+
+    return app
