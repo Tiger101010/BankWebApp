210f4f996c3625c0c21de51c22f28afd444bdf82
Lin <liulinzhi001@gmail.com>
2022-05-02 11:39:48 -0700

    Add pre and post check for deposit/withdraw

--- flaskr/bank.py
+++ flaskr/bank.py
@@ -14,6 +14,7 @@ from werkzeug.security import generate_password_hash
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+from flaskr.helper.util import validate_balance
 
 bp = Blueprint("bank", __name__)
 
@@ -28,15 +29,11 @@ def index():
 
 # Balance Query
 @bp.route("/<usrname>/balance", methods=["GET"])
-def get_balance(usrname):
+@login_required
+def get_balance_by_user(usrname):
     """get balance based on username
     """
-    print("get_balance")
-    db = get_db()
-    balance = db.execute("Select balance FROM user WHERE username = ?", (usrname,)).fetchone()
-    # print(balance)
-    if balance:
-        return str(balance[0])
+    return get_balance(usrname)
 
 # Balance deposit / withdraw
 @bp.route("/<usrname>/balance", methods=["POST"])
@@ -51,19 +48,49 @@ def adjust_balance(usrname):
     type = request.form["adjust_balance"]
     balance = db.execute("Select balance FROM user WHERE username = ?", (usrname,)).fetchone()
 
+    # error msg
+    error = None
+
+    #success msg
+    success = None
+
     amount = request.form["amount"]
+    if not validate_balance(amount):
+        error = "Invalid Amount."
+
     amount = float(amount)
+    
     if type == "deposit":
         new_amount = balance[0] + amount
+        if new_amount > 4204967295.99:
+            error = "Invalid Amount. Amount Overflow"
+        else:
+            success = "Deposit Successful"
 
     elif type == "withdraw":
         new_amount = balance[0] - amount
         if new_amount < 0:
-            #error
-            pass
-        pass
+            error = "Insufficient Balance"
+        else:
+            success = "Withdraw Successful"
 
-    db.execute("Update user SET balance = ? WHERE username = ?", (new_amount,usrname,))
-    db.commit()
+    if error is None:
+        db.execute("Update user SET balance = ? WHERE username = ?", (new_amount,usrname,))
+        db.commit()
+        flash(success)
+
+    else:
+        flash(error)
 
     return redirect(url_for("bank.index"))
+
+
+def get_balance(usrname):
+    """get balance based on username
+    """
+    print("get_balance")
+    db = get_db()
+    balance = db.execute("Select balance FROM user WHERE username = ?", (usrname,)).fetchone()
+    # print(balance)
+    if balance:
+        return str(balance[0])
--- flaskr/helper/util.py
+++ flaskr/helper/util.py
@@ -5,8 +5,7 @@ import uuid
 def validate_string(str_input):
     return re.match("(^[_\\-\\.0-9a-z]{1,127}$)", str_input) != None and 0<len(str_input)<128
 
-
-def validate_register_balance(num_input):
+def validate_balance(num_input):
     return re.match("^(0|([1-9][0-9]*))((\.[0-9]{1,2}))$", num_input) != None and (0 <= float(num_input)<=4294967295.99)
 
 def create_random_userid():
